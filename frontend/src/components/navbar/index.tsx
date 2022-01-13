da26805bed59e5d7d2b0097923259006e7868112
Alan PIovezan <alan.log.piovezan@gmail.com>
2022-01-12 21:59:10 -0300

    change gitrepo navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -9,7 +9,7 @@ function NavBar(){
         <nav className="container">
           <div className="dsmovie-nav-content">
             <h1>DSMOVIE</h1>
-            <a href="https://github.com/piovezandev">
+            <a href="https://github.com/piovezandev/dsmovie">
               <div className="dsmovie-contact-container">
               <GitHubIcon />
               <p className="dsmovie-contact-link">/devsuperior</p>
